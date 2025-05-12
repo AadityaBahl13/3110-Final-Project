184a1f31f2ba9a884840cb806f4098d6b9d5d88b
Jonah Bernard <jb2528@cornell.edu>
2025-05-12 17:07:33 -0400

    Fixed some typos in lib/dune file

---
+++ test/test_perceptron.ml
@@ -0,0 +1,23 @@
+open OUnit2
+open Finalproject.Lin_alg
+open Finalproject.Data
+open Finalproject.Perceptron
+open QCheck
+
+
+let init_test = failwith "not_implemented"
+
+let train_test = failwith "not_implemented"
+
+let predict_test = failwith "not_implemented"
+
+let step_test = failwith "not_implemented"
+
+let get_weight = failwith "not_implemented"
+
+let get_bias = failwith "not_implemented"
+
+let tests =
+  [
+
+  ]
