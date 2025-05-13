7b965f212a130bd3778155d7e71529e1659da6e5
Danielle Imogu <dannieim689@gmail.com>
2025-05-13 01:57:03 -0400

    added "linear" tester and edited scaling

--- bin/main.ml
+++ bin/main.ml
@@ -43,16 +43,64 @@ let main () =
   | _ -> print_endline "An unknown error occurred."
 
 (* ---------- Utilities ---------- *)
-let transform (x, y) ~w ~h =
+(* let transform (x, y) ~w ~h =
   let scale = 10 in
   let x' = (x * scale) + (w / 2) in
   let y' = (h / 2) - (y * scale) in
-  (x', y')
+  (x', y') *)
 
-let draw_axes w h renderer =
+let transform_f (x, y) ~w ~h =
+  let xf = (float_of_int w /. 2.) +. (x *. 20.) in
+  let yf = (float_of_int h /. 2.) -. (y *. 20.) in
+  (int_of_float xf, int_of_float yf)
+
+let get_data_bounds data =
+  let xs, ys =
+    List.fold_left
+      (fun (xs, ys) (xv, _) ->
+        match to_list xv |> List.hd with
+        | [ x; y ] -> (x :: xs, y :: ys)
+        | _ -> (xs, ys))
+      ([], []) data
+  in
+  let min_x = List.fold_left min max_int xs in
+  let max_x = List.fold_left max min_int xs in
+  let min_y = List.fold_left min max_int ys in
+  let max_y = List.fold_left max min_int ys in
+  ( float_of_int min_x,
+    float_of_int max_x,
+    float_of_int min_y,
+    float_of_int max_y )
+
+let create_transform ~w ~h data =
+  let min_x, max_x, min_y, max_y = get_data_bounds data in
+  let margin = 50. in
+  let data_width = max_x -. min_x in
+  let data_height = max_y -. min_y in
+  let scale_x = (float_of_int w -. (2. *. margin)) /. data_width in
+  let scale_y = (float_of_int h -. (2. *. margin)) /. data_height in
+  let scale = Float.min scale_x scale_y in
+  let center_x = (min_x +. max_x) /. 2. in
+  let center_y = (min_y +. max_y) /. 2. in
+  fun (x, y) ->
+    let xf = (float_of_int w /. 2.) +. ((x -. center_x) *. scale) in
+    let yf = (float_of_int h /. 2.) -. ((y -. center_y) *. scale) in
+    (int_of_float xf, int_of_float yf)
+
+let draw_axes renderer ~w ~h ~data =
+  let transform = create_transform ~w ~h data in
+  let min_x, max_x, min_y, max_y = get_data_bounds data in
   let axis_color = Draw.opaque Draw.black in
-  Draw.line ~color:axis_color ~x0:0 ~y0:(h / 2) ~x1:w ~y1:(h / 2) renderer;
-  Draw.line ~color:axis_color ~x0:(w / 2) ~y0:0 ~x1:(w / 2) ~y1:h renderer
+
+  (* Draw x-axis from (min_x, 0) to (max_x, 0) *)
+  let x0, y0 = transform (min_x, 0.) in
+  let x1, y1 = transform (max_x, 0.) in
+  Draw.line ~color:axis_color ~x0 ~y0 ~x1 ~y1 renderer;
+
+  (* Draw y-axis from (0, min_y) to (0, max_y) *)
+  let x0, y0 = transform (0., min_y) in
+  let x1, y1 = transform (0., max_y) in
+  Draw.line ~color:axis_color ~x0 ~y0 ~x1 ~y1 renderer
 
 let make_plot_area (table : (tensor * label) list) =
   let width, height = (400, 400) in
@@ -62,13 +110,14 @@ let make_plot_area (table : (tensor * label) list) =
   try
     let area = Widget.get_sdl_area widget in
     let draw_fun renderer =
-      draw_axes width height renderer;
-
+      (* draw_axes width height renderer; *)
       List.iter
         (fun (key, label_value) ->
           match to_list key |> List.hd with
           | x :: y :: _ ->
-              let x', y' = transform (x, y) ~w:width ~h:height in
+              let x', y' =
+                transform_f (float_of_int x, float_of_int y) ~w:width ~h:height
+              in
               let color = Draw.set_alpha 255 (color_of_label label_value) in
               Draw.circle ~color ~radius:5 ~x:x' ~y:y' renderer
           | _ -> ())
@@ -91,39 +140,66 @@ let current_data = ref ([] : (tensor * label) list)
 let current_table = ref init_data
 
 (* --------- Perceptron --------- *)
-let draw_points renderer =
+(* let draw_points renderer =
   draw_axes width height renderer;
   List.iter
     (fun (xv, label) ->
       match to_list xv |> List.hd with
       | [ x; y ] ->
-          let x', y' = transform (x, y) ~w:width ~h:height in
+          let x', y' =
+            transform_f (float_of_int x, float_of_int y) ~w:width ~h:height
+          in
           let color = Draw.opaque (color_of_label label) in
           Draw.circle ~color ~radius:4 ~x:x' ~y:y' renderer
       | _ -> ())
-    !current_data
+    !current_data *)
 
-let draw_decision_boundary weights renderer =
+let draw_points renderer =
+  draw_axes ~w:width ~h:height ~data:!current_data renderer;
+  let transform = create_transform ~w:width ~h:height !current_data in
+  List.iter
+    (fun (xv, label) ->
+      match to_list xv |> List.hd with
+      | [ x; y ] ->
+          let x', y' = transform (float_of_int x, float_of_int y) in
+          let color = Draw.opaque (color_of_label label) in
+          Draw.circle ~color ~radius:4 ~x:x' ~y:y' renderer
+      | _ -> ())
+    !current_data;
+  let cx, cy = (width / 2, height / 2) in
+  Draw.circle ~color:(Draw.opaque Draw.green) ~radius:3 ~x:cx ~y:cy renderer
+
+let compute_decision_boundary_points w1 w2 b min_x max_x =
+  let y x = -.((w1 *. x) +. b) /. w2 in
+  let x1 = min_x in
+  let y1 = y x1 in
+  let x2 = max_x in
+  let y2 = y x2 in
+  ((x1, y1), (x2, y2))
+
+let draw_decision_boundary weights bias renderer =
+  let b = float_of_int bias in
+  let data = !current_data in
   match weights with
-  | w0 :: w1 :: w2 :: _ ->
-      let x1 = -20. in
-      let x2 = 20. in
-      let y1 = -.(w0 +. (w1 *. x1)) /. w2 in
-      let y2 = -.(w0 +. (w1 *. x2)) /. w2 in
-      let x1', y1' =
-        transform (int_of_float x1, int_of_float y1) ~w:width ~h:height
+  | [ w1; w2 ] ->
+      let min_x, max_x, _, _ = get_data_bounds data in
+      let (x1, y1), (x2, y2) =
+        compute_decision_boundary_points w1 w2 b min_x max_x
       in
-      let x2', y2' =
-        transform (int_of_float x2, int_of_float y2) ~w:width ~h:height
-      in
+      let transform = create_transform ~w:width ~h:height data in
+      let px1, py1 = transform (x1, y1) in
+      let px2, py2 = transform (x2, y2) in
+
       let color = Draw.opaque Draw.green in
-      Draw.line ~color ~x0:x1' ~y0:y1' ~x1:x2' ~y1:y2' renderer
+      Draw.line ~color ~x0:px1 ~y0:py1 ~x1:px2 ~y1:py2 renderer
   | _ -> ()
 
-let update_canvas ?weights area () =
+let update_canvas ?weights bias area () =
   Sdl_area.clear area;
   Sdl_area.add area draw_points;
-  Option.iter (fun w -> Sdl_area.add area (draw_decision_boundary w)) weights
+  Option.iter
+    (fun w -> Sdl_area.add area (draw_decision_boundary w bias))
+    weights
 
 let run_training_stepwise steps_label =
   let perceptron = init !current_table 100 in
@@ -134,24 +210,46 @@ let run_training_stepwise steps_label =
   let idx = ref 0 in
 
   let rec step_graph () =
-    if !finished then ()
+    if !finished then ignore (W.set_text steps_label "Done!")
     else if !idx >= Array.length data_array then finished := true
     else
       let input, label = data_array.(!idx) in
       incr idx;
       let updated = step perceptron input label in
+      incr steps;
+      W.set_text steps_label ("Steps: " ^ string_of_int !steps);
       if not updated then (
-        incr steps;
         let weights =
           to_list (get_weight perceptron) |> List.hd |> List.map float_of_int
         in
-        update_canvas ~weights area ();
-        W.set_text steps_label ("Steps: " ^ string_of_int !steps));
-      ignore (Timeout.add 1 step_graph)
+        List.iteri
+          (fun i w ->
+            print_endline
+              ("Weight(step) " ^ string_of_int i ^ ": " ^ string_of_float w))
+          weights;
+        update_canvas ~weights (get_bias perceptron) area ());
+      ignore (Timeout.add 5 step_graph)
   in
-  ignore (Timeout.add 1 step_graph)
+  ignore (Timeout.add 2 step_graph)
 
-(* let run_training_final label = *)
+let run_training_final steps_label =
+  let perceptron = init !current_table 100 in
+  train perceptron;
+  let weights =
+    let w = to_list (get_weight perceptron) in
+    match w with
+    | [] -> []
+    | hd :: _ ->
+        List.iteri
+          (fun i w ->
+            print_endline
+              ("Weight(final) " ^ string_of_int i ^ ": " ^ string_of_int w))
+          hd;
+        List.map float_of_int
+          hd (* Adjust this depending on your perceptron's weight structure *)
+  in
+  update_canvas ~weights (get_bias perceptron) area ();
+  W.set_text steps_label "Final result"
 
 let visualize_perceptron () =
   let steps_label = W.label "Steps: 0" in
@@ -161,7 +259,11 @@ let visualize_perceptron () =
         run_training_stepwise steps_label)
   in
   let btn_final =
-    W.button "Train Fully" ~action:(fun _ -> print_endline "Fully Train")
+    W.button "Train Fully" ~action:(fun _ -> run_training_final steps_label)
+  in
+
+  let btn_back =
+    W.button "Back To Main" ~action:(fun _ -> current_screen := MainMenu)
   in
 
   Sdl_area.add area draw_points;
@@ -170,38 +272,13 @@ let visualize_perceptron () =
     L.tower
       [
         L.resident canvas;
-        L.flat_of_w [ btn_steps; btn_final ];
+        L.flat_of_w [ btn_steps; btn_final; btn_back ];
         L.resident steps_label;
       ]
   in
   layout
 
 (* ---------- Screens ---------- *)
-
-(* let visualize_perceptron () = let width, height = (400, 400) in let widget =
-   W.sdl_area ~w:width ~h:height () in let area = W.get_sdl_area widget in
-
-   let draw_points renderer = draw_axes width height renderer; List.iter (fun
-   (xv, label) -> match to_list xv |> List.hd with | [ x; y ] -> let x', y' =
-   transform (x, y) ~w:width ~h:height in let color = Draw.opaque
-   (color_of_label label) in Draw.circle ~color ~radius:4 ~x:x' ~y:y' renderer |
-   _ -> ()) !current_data in Sdl_area.add area draw_points;
-
-   let step_counter = ref 0 in let steps_label = W.label ("Steps: " ^
-   string_of_int !step_counter) in
-
-   let update_plot_and_boundary weights = let draw_boundary renderer = match
-   weights with | w0 :: w1 :: w2 :: _ -> let x1 = -20. in let x2 = 20. in let y1
-   = -.(w0 +. (w1 *. x1)) /. w2 in let y2 = -.(w0 +. (w1 *. x2)) /. w2 in let
-   x1', y1' = transform (int_of_float x1, int_of_float y1) ~w:width ~h:height in
-   let x2', y2' = transform (int_of_float x2, int_of_float y2) ~w:width
-   ~h:height in let color = Draw.opaque Draw.green in Draw.line ~color ~x0:x1'
-   ~y0:y1' ~x1:x2' ~y1:y2' renderer | _ -> () in Sdl_area.clear area;
-   Sdl_area.add area draw_points; Sdl_area.add area draw_boundary in let
-   train_button = W.button "Start Training" ~action:(fun _ -> let perceptron =
-   init !current_table 1000 in let steps = ref 0 in let _ = train perceptron
-   (fun weights -> steps := !steps + 1; step_counter := !steps W.)) in
-   L.tower_of_w [ widget; train_button; steps_label ] *)
 
 let build_screen scr =
   match scr with
